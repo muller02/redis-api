4fa538b03462414df5ca5b3785ceaf204916e928
daback <daback93@gmail.com>
2024-08-13 12:06:21 +0900

    Add: redis와 UserDTO를 직렬화하기 위해 Serializable 상속 추가

--- src/main/java/com/api/redis_client/model/UserDTO.java
+++ src/main/java/com/api/redis_client/model/UserDTO.java
@@ -1,6 +1,8 @@
 package com.api.redis_client.model;
 
-public class UserDTO {
+import java.io.Serializable;
+
+public class UserDTO implements Serializable{
     private String userId;
     private String gender;
     private String birthYear;
